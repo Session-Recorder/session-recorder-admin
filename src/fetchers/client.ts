4d93b740f5d91622577f97b66bb5cff168844b8a
tunatone0111 <tunatone.0111@gmail.com>
2021-02-28 21:58:41 +0900

    [refactor] move baseURL to .env.local

--- src/fetchers/client.ts
+++ src/fetchers/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosClient = axios.create({
-  baseURL: "http://sebastianrcnt.iptime.org:3000",
+	baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 export const fetcher = (url) => AxiosClient.get(url).then((res) => res.data);
